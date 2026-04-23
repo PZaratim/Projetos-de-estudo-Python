d61217d94d46ef495ba71de30f5ff8b865520f60
Pedro H. Zaratim <zaratimpedro1@gmail.com>
2026-04-23 15:52:00 -0300

    Add files via upload

---
+++ Calculadora_Idade.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome:")
+data_nascimento = int(input("Digite seu ano de nascimento:"))
+ano_atual = 2026
+idade = ano_atual - data_nascimento
+
+print("Olá", nome, "sua idade é", idade)
